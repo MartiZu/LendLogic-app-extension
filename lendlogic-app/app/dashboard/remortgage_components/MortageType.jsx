8d0176c934aa8120b655f850fb9daa48b62bbb39
Martina Zurli || Junior Web Developer <137444422+MartiZu@users.noreply.github.com>
2023-12-21 00:41:02 +0100

    Update MortageType.jsx

--- lendlogic-app/app/dashboard/remortgage_components/MortageType.jsx
+++ lendlogic-app/app/dashboard/remortgage_components/MortageType.jsx
@@ -1,107 +1,127 @@
-import React from "react";
+import React, { use } from "react";
 import { useState, useEffect } from "react";
 
-const MortageType = ({ value, q2 }) => {
+export default function MortageType({ value, q2 }) {
   // Pull mortage type from database
   const {
-    loanLength,
     loanAmount,
-    userMonthlyPayment,
     userInterestRate,
     mortgageType,
     fixedTermLength,
     startDate,
   } = value;
+  //seacrh state
+  const [searchInput, setSearchInput] = useState("");
+  const [remainingYears, setRemainingYears] = useState(0);
+  const [remainingMonths, setRemainingMonths] = useState(0);
 
-  let yearsRemaining = 0;
-  let monthsRemaining = 0;
-  let daysRemaining = 0;
+  //handle input search
+  const handleSearchInputChange = (e) => {
+    const input = e.target.value;
+    setSearchInput(input);
+    // const isValidNumber = /^[0-9]+$/.test(input);
+    // if (isValidNumber) {
+    //   setSearchInput(input);
+    // } else {
+    //   alert("Please enter a valid number");
+    // }
+  };
+  const handleSearchButtonClick = () => {
+    //ensure that searchInput is a valid number
+    const percentage = parseFloat(searchInput);
 
-  const [monthlyPayment, setMonthlyPayment] = useState(userMonthlyPayment);
-  const [interestRate, setInterestRate] = useState(userInterestRate);
-  const [loanTerm, setLoanTerm] = useState(loanLength);
+    if (!isNaN(percentage)) {
+      //convert remaining years and months to monthss
+      const totalMonthsRemaining = remainingYears * 12 + remainingMonths;
 
-
-  function remainingFixedTermTime() {
+      //calculate Early Repayment Charge based on the percentage the user had entered
+      const earlyRepaymentCharge =
+        Math.floor((percentage / 100) * loanAmount * totalMonthsRemaining) / 12;
+      //calculate savings based on userInterestRate for the same period
+      const savings =
+        Math.floor(
+          (userInterestRate / 100) * loanAmount * totalMonthsRemaining
+        ) / 12;
+      console.log("savings", savings);
+      //display payment required to change mortgage type and savings
+      alert(
+        `Early Repayment Charge: £ ${earlyRepaymentCharge} and you will save ${savings}`
+      );
+    } else {
+      alert("Please enter a valid percentage.");
+    }
+  };
+  //calculate remaining time, taking today's date and the start date, calculate an avarage
+  const remainingFixedTermTime = () => {
     // Work out the difference between the current date and the start date
     const currentDate = new Date();
     const startDateObj = new Date(startDate); // Assuming startDate is in a valid date format
-    console.log("line 32 mortageg type", currentDate);
-    console.log("line 33 mortageg type", startDateObj);
-
     const timeDifference = startDateObj - currentDate;
-    console.log("line 36", timeDifference);
 
+    // start date is in the past, adjust the calculation to get a positive value
+    const adjustedTimeDifference = Math.abs(timeDifference);
+    console.log("adjusted time difference", adjustedTimeDifference);
     // Convert the time difference from milliseconds to years, months, and days
-    yearsRemaining = Math.floor(timeDifference / (1000 * 3600 * 24 * 365));
-    monthsRemaining = Math.floor(
-      (timeDifference % (1000 * 3600 * 24 * 365)) / (1000 * 3600 * 24 * 30)
-    );
-    daysRemaining = Math.floor(
-      (timeDifference % (1000 * 3600 * 24 * 30)) / (1000 * 3600 * 24)
-    );
-    console.log("line 42", yearsRemaining);
-    console.log("line 43", monthsRemaining);
-    console.log("line 44", daysRemaining);
+    let years = Math.floor(adjustedTimeDifference / (1000 * 3600 * 24 * 365));
+    let months = Math.floor(adjustedTimeDifference / (1000 * 3600 * 24 * 30));
+    console.log("remaining months", remainingMonths);
+    // Calculate the fixed term length in years
+    const fixedTermYears = parseInt(fixedTermLength.match(/\d+/)[0], 10);
+    // Calculate the remaining time in years
+    const yearsRemaining = Math.max(fixedTermYears - years, 0);
+    const monthsRemaining = Math.max(12 - months, 0);
+    setRemainingYears(yearsRemaining);
+    setRemainingMonths(monthsRemaining);
+  };
 
-    // Display the remaining fixed term in years, months, and days
-    // console.log(`Remaining Fixed Term: ${yearsRemaining} years, ${monthsRemaining} months, ${daysRemaining} days`);
-  }
-
-  remainingFixedTermTime();
+  useEffect(() => {
+    remainingFixedTermTime();
+  }, []);
 
   return (
     <div className="mt-8 mx-4 text-center text-2xl">
-      <div className="p-2 font-normal text-xl">
-        Your current plan has a fixed rate length of{" "}
+      <p className="py-2 font-normal text-xl">
+        {" "}
+        You have{" "}
         <span className="text-2xl font-bold text-purple-accent">
-          {fixedTermLength}
+          {remainingYears} years and {remainingMonths} months{" "}
         </span>{" "}
-        and your remaning balance is{" "}
+        left on your fixed term, with a fixed rate of{" "}
+        <span className="text-2xl font-bold text-purple-accent">
+          {userInterestRate}%
+        </span>
+        and a remianing balance of{" "}
         <span className="text-2xl font-bold text-purple-accent">
           £{loanAmount}
         </span>
-      </div>
-      <div id="toggles" className="flex flex-col items-center">
-        <label className="py-2 p-2 font-normal text-xl" htmlFor="interestRate">
-          Your interest rate is:{" "}
-          <span className="text-2xl font-bold text-purple-accent">
-            {interestRate}%
-          </span>
-        </label>
-        <label className="py-2 p-2 font-normal text-xl" htmlFor="interestRate">
-          The current market avarage interest rate is{" "}
-          <span className="text-2xl font-bold text-purple-accent">2.9%</span>
-        </label>
-        <div>
-          {" "}
-          <p className="py-2 font-normal text-xl">
-            What is the early repayment charge? You have left on your fixed term:{" "}
-            <span className="text-2xl font-bold text-purple-accent">
-              {yearsRemaining} years, {monthsRemaining} months, {daysRemaining}{" "}
-              days
-            </span>
-          </p>
-          <p className="py-2 font-normal text-xl">Please enter the repayment percentage here</p>
-          {/* <div className="flex flex-row w-72 justify-center py-3">
-            <input
-              type="text"
-              placeholder="Enter number"
-              value={searchInput}
-              onChange={handleSearchInputChange}
-              className="w-40 h-12 p-2 border rounded-l-full text-xl focus:outline-none"
-            />
-            <button
-              onClick={handleSearchButtonClick}
-              className="w-24 h-12 bg-purple-accent rounded-r-full text-off-white text-xl font-semibold shadow-button"
-            >
-              Search
-            </button>
-          </div> */}
-        </div>{" "}
+      </p>
+      <p className="py-2 font-normal text-xl">
+        The current market avarage interest rate is{" "}
+        {/*hardcoded interest rate*/}
+        <span className="text-2xl font-bold text-purple-accent">2.9%</span>
+      </p>
+      <div className="flex flex-col items-center">
+        <p className="py-2 font-normal text-xl">
+          Thinking about an early repayment charge? Enter the repayment
+          percentage number here
+        </p>
+        <div className="flex flex-row w-72 justify-center py-3">
+          <input
+            type="text"
+            placeholder="Enter number"
+            value={searchInput}
+            onChange={handleSearchInputChange}
+            className="w-32 h-12 p-2 border rounded-l-full text-center text-base focus:outline-none"
+          />
+          <button
+            onClick={handleSearchButtonClick}
+            className="w-20 h-12 bg-purple-accent rounded-r-full text-off-white text-base font-semibold shadow-button"
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
-};
+}
 
-export default MortageType;
