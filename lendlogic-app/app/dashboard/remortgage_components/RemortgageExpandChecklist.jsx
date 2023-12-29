5ea1289ebc1fdd154d52c43e223b7066fb8bd992
MartiZu <martina.zurli@gmail.com>
2023-12-29 23:21:33 +0100

    changed to checklist for remortgage user

---
+++ lendlogic-app/app/dashboard/remortgage_components/RemortgageExpandChecklist.jsx
@@ -0,0 +1,48 @@
+export default function RemortgageExpandChecklist() {
+  return (
+    <>
+      <a
+        target="_blank"
+        className="bg-off-white text-lg font-normal flex items-center justify-center h-16 p-3 border-2 border-purple-accent rounded-full shadow-button"
+        href="https://www.gov.uk/guidance/how-to-remortgage-your-help-to-buy-home-without-borrowing-more-money#making-changes-to-your-equity-loan"
+      >
+        Making changes to your equity loan
+      </a>
+      <a
+        target="_blank"
+        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        href="https://www.gov.uk/guidance/how-to-remortgage-your-help-to-buy-home-without-borrowing-more-money#negative-equity"
+      >
+        Negative equity
+      </a>
+      <a
+        target="_blank"
+        className="bg-off-white flex items-center justify-center h-16 text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        href="https://www.gov.uk/guidance/how-to-remortgage-your-help-to-buy-home-without-borrowing-more-money#remortgaging-with-a-current-or-new-lender"
+      >
+        Remortgaging with a new lender
+      </a>
+      <a
+        target="_blank"
+        className="bg-off-white flex items-center justify-center h-16 text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        href="https://www.gov.uk/government/publications/repay-your-help-to-buy-equity-loan"
+      >
+        Repay your Help to Buy: Equity Loan
+      </a>
+      <a
+        target="_blank"
+        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        href="https://www.gov.uk/guidance/how-to-repay-your-equity-loan-when-you-sell-your-home"
+      >
+        How to repay your equity loan when you sell your home
+      </a>
+      <a
+        target="_blank"
+        className="flex justify-center items-center h-16 bg-off-white text-lg font-normal p-3 border-2 border-purple-accent rounded-full shadow-button"
+        href="https://www.gov.uk/guidance/how-to-repay-your-equity-loan-when-you-remortgage"
+      >
+        How to repay your equity loan when you remortgage
+      </a>
+    </>
+  );
+}
